fde58615cb1c757d1aa98d517f5f6751369e1a8a
ETCasual <casualorient@gmail.com>
2022-04-07 03:48:42 +0800

    Removed unused useReactionString hook

--- packages/client/src/hooks/useReactionString.ts
+++
@@ -1,8 +0,0 @@
-import create from 'zustand';
-import { combine } from 'zustand/middleware';
-
-export const useSearch = create(
-  combine({ searchString: '' }, (set) => ({
-    setSearchString: (searchString: string) => set({ searchString }),
-  }))
-);
