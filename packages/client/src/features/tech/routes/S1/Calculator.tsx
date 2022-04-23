d50a111ba7248727323445647cad27d023abab58
Marcus Lee <marcustutorial@hotmail.com>
2022-04-23 14:15:14 +0800

    fixed task 1

--- packages/client/src/features/tech/routes/S1/Calculator.tsx
+++ packages/client/src/features/tech/routes/S1/Calculator.tsx
@@ -53,7 +53,7 @@ export const S1Calculator: FunctionComponent = () => {
     setSeq(
       team.data.User.filter((u) => u.identityCardNumber !== team.data?.leaderId).findIndex(
         (u) => u.identityCardNumber === user.identityCardNumber
-      ) + 5
+      ) + 1
     );
   }, [navigate, team, user]);
 
